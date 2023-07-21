43d993da7976db49a66213ba92fb9cb8a66f9f88
Liam Mckenzie <mckeld3@student.op.ac.nz>
2023-07-21 14:13:17 +1200

    ADD: Script to detect swipe up

---
+++ Assets/Scripts/SwipeDetect.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeDetect : MonoBehaviour
+{
+    public Vector3 mouseDelta = Vector3.zero;
+
+    private Vector3 lastMousePosition = Vector3.zero;
+
+    public float swipeSensitivity = 0.3f;
+    public bool isSwipingUp = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseDelta = (Input.mousePosition - lastMousePosition) * Time.deltaTime;
+
+        lastMousePosition = Input.mousePosition;   
+
+        
+        isSwipingUp = (mouseDelta.y > swipeSensitivity);
+        
+    }
+}
