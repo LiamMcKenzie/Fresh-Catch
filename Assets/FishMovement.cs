9b2c067a3f4a9cd7cad06a4f12f0abc717bbe009
Liam Mckenzie <mckeld3@student.op.ac.nz>
2023-07-22 17:32:37 +1200

    ADD: rotate fish in target direction, refactor FishMovement.cs

--- Assets/FishMovement.cs
+++ Assets/FishMovement.cs
@@ -6,29 +6,49 @@ public class FishMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float swimInterval = 3f;
+    public float rotationSpeed = 5f;
 
     private Rigidbody rb;
     public float timer;
-    public Vector3 randomDirection;
+    public Quaternion targetRotation;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         timer = swimInterval;
+        
     }
 
     void Update()
     {
         timer -= Time.deltaTime;
-
+        
         if(timer <= 0f)
         {
-            randomDirection = Random.insideUnitSphere.normalized;
-            randomDirection.y = 0f;
+            StartCoroutine(RandomMovementCoroutine());
+        }
+    }
 
-            timer = swimInterval;
+    IEnumerator RandomMovementCoroutine()
+    {
+        timer = swimInterval;
+        
+        //randomizes a new direction
+        float randomRotationY = Random.Range(0f, 360f);
+        targetRotation = Quaternion.Euler(0f, randomRotationY, 0f);
+
+        //rotates towards the random direction
+        while (Quaternion.Angle(transform.rotation, targetRotation) > 0.1f)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            yield return null;
         }
 
-        rb.velocity = randomDirection * moveSpeed;
+        //waits a second before moving
+        yield return new WaitForSeconds(1f);
+
+        //moves forwards in the random direction
+        Vector3 forwardDirection = transform.forward;
+        rb.velocity = forwardDirection.normalized * moveSpeed;        
     }
 }
