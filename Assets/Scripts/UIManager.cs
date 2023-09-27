04cf4f68f18470c9648cfec16a91198e475c0b4f
Liam Mckenzie <mckeld3@student.op.ac.nz>
2023-09-27 16:53:32 +1300

    ADD: UI manager script

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIManager : MonoBehaviour
+{
+    public float fadeTime = 1f;
+
+    public RectTransform rectTransform;
+
+    public void PanelFadeIn()
+    {
+        //rectTransform.DOScale(0,1).SetEase( Ease.InBounce );
+        rectTransform.transform.localPosition = new Vector3(0f, -1000f, 0f);
+        rectTransform.DOAnchorPos(new Vector2(0f, 0f), fadeTime, false).SetEase(Ease.OutElastic);
+    }
+
+    public void PanelFadeOut()
+    {
+        //RectTransform.transform.localPosition = new Vector3(0f, 0f, 0f);
+        //DOTweenModuleUI.DOAnchorPos(new Vector2(0f, -1000f), fadeTime, false).SetEase(Ease.InOutQuint);
+    }
+}
